19a2505a187fd57f94c2a6f9344f599a29bcc7a2
jacquesa <antony.jacques06@gmail.com>
2024-01-06 18:33:21 +0100

    refacto entry point index & clean condition useless

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ function GenerateGame() {
 
     const allFile = DirectoryParser()
     allFile.forEach((file) => {
-
+        console.log("Start New Game")
         const newGameObject = LineReader(FileReader(file))
 
         if (newGameObject.carte) {
@@ -16,19 +16,13 @@ function GenerateGame() {
                 newGameObject.carte
             )
 
-            if (newGameObject.montagne.length) {
-                newGameObject.montagne.forEach((each) => map.initMontagneTile(each))
-            }
+            newGameObject.montagne.forEach((each) => map.initMontagneTile(each))
+            newGameObject.treasure.forEach((each) => map.initTreasureTile(each))
+            newGameObject.aventurier.forEach((each) => map.initAdventurerTile(each))
 
-            if (newGameObject.treasure.length) {
-                newGameObject.treasure.forEach((each) => map.initTreasureTile(each))
-            }
-
-            if (newGameObject.aventurier.length) {
-                newGameObject.aventurier.forEach((each) => map.initAdventurerTile(each))
-            }
 
             map.startAdventure()
+            console.log(map.mapAdventurer)
             console.log(map.mapTreasure)
         }
 
